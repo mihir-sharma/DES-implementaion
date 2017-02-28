fe5f29242a02d8b73fce772745424a2b821d8072
mihir-sharma <mihir.sharma@st.niituniversity.in>
2017-02-28 14:25:25 +0530

    Update subkey generation.py

--- subkey generation.py
+++ subkey generation.py
@@ -36,15 +36,31 @@ def key_rotator(L,R,keyno):
             LN,RN = key_rotator(LN,RN,keyno+1)
     return LN, RN
 
+def recombiner(left,right):
+    recomb=[2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9]
+    recomb = left + right
+    return recomb
+
+def PC2_permutator(rotkey):
+    subkey = [2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7]
+    PC2=[14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
+    for i in range(0,48):
+        index = PC2[i]
+        subkey[i] = rotkey[index-1]
+    return subkey
+
 yi = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
 leftkey=[2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9]
 rightkey=[2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9]
+recombkey = [2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9,2,3,4,5,6,7,8,9]
+subkey = [2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7,2,3,4,5,6,7]
 rotator = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 y = input("enter permuted key")
 left,right =key_splitter(y)
 for h in range(0,16):
     leftkey[h],rightkey[h]=key_rotator(left,right,rotator[h])
-    print(leftkey[h])
-    print(rightkey[h])
-    print("\n\n")
+    recombkey[h] = recombiner(leftkey[h],rightkey[h])
+    subkey[h] = PC2_permutator(recombkey[h])
+    print(recombkey[h])
+
 
